843bb5ce076aa506435f0a76a885af3a714b017e
fadi quader <fadi.quader@gmail.com>
2018-11-18 01:24:05 +0200

    try new hooks

--- packages/web/src/routes/Landing/Landing.js
+++ packages/web/src/routes/Landing/Landing.js
@@ -1,14 +1,30 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 
-class Landing extends React.Component {
-
-  render() {
-    return (
-      <div>
-        <h1>Landing</h1>
-      </div>
-    )
+function counterState() {
+  const [ state, setState ] = useState({ count: 1});
+  const actions = {
+    increment: () => setState({ count: state.count + 1 }),
+    decrment: () => setState({ count: state.count - 1 }),
   }
+  return {
+    state,
+    ...actions
+  }
+}
+const Landing = () => {
+
+  const { state, increment } = counterState();
+  return (
+    <div>
+      <h1>Landing</h1>
+      <div>
+        <h2>Count: {state.count}</h2>
+        <p>
+          <button onClick={increment}>increment</button>
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default Landing;
